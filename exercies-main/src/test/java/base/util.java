ac7f5d72d85fa7c03fbda47486d094cf9da022e4
Supriyapandit <supriyapandit03@gmail.com>
2023-10-04 14:23:57 +0530

    Remove Duplicate Directory

--- exercies-main/src/test/java/base/util.java
+++
@@ -1,9 +0,0 @@
-package base;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-public class util {
-	public static String generateCurrentDateAndTime() {
-        return new SimpleDateFormat("ddMMyyyyHHmmssSSS").format(new Date());
-    }
-}
